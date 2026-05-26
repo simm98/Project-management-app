a544aac1c29ef1d0430771805c22362ccfe808af
simm98 <isaac_mendoza@epam.com>
2026-05-25 19:58:06 -0600

    conftest debug

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,9 +6,9 @@ client = TestClient(app)
 
 @pytest.fixture
 def auth_token():
-    client.post("/auth", data={"login": "user", "password": "1234", "repeat_password": "1234"})
+    r = client.post("/auth", data={"login": "user", "password": "1234", "repeat_password": "1234"})
+    print("Auth response:", r.status_code, r.json())
     response = client.post("/login", data={"username": "user", "password": "1234"})
-    print(response)
     data = response.json()
     print(data)
     return data["access_token"]

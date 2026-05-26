2006d513f5d4c6db6cadf09b85000e5d7d46069f
simm98 <isaac_mendoza@epam.com>
2026-05-25 19:39:26 -0600

    test_main project & auth test case

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,39 +1,20 @@
-# import pytest
-# from fastapi.testclient import TestClient
-# from sqlalchemy import create_engine
-# from sqlalchemy.orm import sessionmaker
-# from app.main import app, get_db
-# from app.database import Base
+import pytest
+from fastapi.testclient import TestClient
+from app.main import app
 
-# # Usamos SQLite en memoria para pruebas rápidas
-# SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+client = TestClient(app)
 
-# engine = create_engine(
-#     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-# )
-# TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+@pytest.fixture
+def auth_token():
+    client.post("/auth", json={
+        "login": "user",
+        "password": "1234",
+        "repeat_password": "1234"
+    })
+    response = client.post("/login", json={
+        "login": "user",
+        "password": "1234"
+    })
+    data = response.json()
+    return data["access_token"]
 
-# # Crear las tablas en la DB de prueba
-# Base.metadata.create_all(bind=engine)
-
-# # Fixture de sesión de DB
-# @pytest.fixture(scope="function")
-# def db_session():
-#     session = TestingSessionLocal()
-#     try:
-#         yield session
-#     finally:
-#         session.close()
-
-# # Override de la dependencia get_db para usar la DB de prueba
-# @pytest.fixture(scope="function")
-# def client(db_session):
-#     def override_get_db():
-#         try:
-#             yield db_session
-#         finally:
-#             db_session.close()
-
-#     app.dependency_overrides[get_db] = override_get_db
-#     yield TestClient(app)
-#     app.dependency_overrides.clear()
--- tests/test_main.py
+++ tests/test_main.py
@@ -4,15 +4,17 @@ from app.main import app
 
 client = TestClient(app)
 
-
-client = TestClient(app)
-
-def test_get_projects_list():
-    response = client.get("/projects")
+def test_get_projects_list(auth_token):
+    headers = {"Authorization": f"Bearer {auth_token}"}
+    response = client.post("/projects", json={
+        "name": "Proyecto Demo",
+        "description": "Este es un proyecto de prueba"
+    }, headers=headers)
     assert response.status_code == 200
-    assert isinstance(response.json(), list)
+    assert response.json()["name"] == "Proyecto Demo"
 
-def test_get_project_not_found():
-    response = client.get("/projects/999/info")
+def test_get_project_not_found(auth_token):
+    headers = {"Authorization": f"Bearer {auth_token}"}
+    response = client.get("/projects/999/info", headers=headers)
     assert response.status_code == 404
     assert response.json()["detail"] == "Project not found"
